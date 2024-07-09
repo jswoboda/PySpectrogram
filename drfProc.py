a0670b77894b2453f46f8f25aafb234ec9dcade9
John Swoboda <swoboj@mit.edu>
2024-07-09 12:59:14 -0400

    updates to pyspectrogram

--- drfProc.py
+++ drfProc.py
@@ -1,5 +1,5 @@
 # ============================================================================================================
-#     Code: AudioProcessor.py
+#     Code: DrfProcessor.py
 #     Author: Casey R. Densmore
 #     
 #     Purpose: Handles all signal processing functions related to accessing microphones + converting PCM data to spectra
@@ -7,7 +7,7 @@
 #   General Functions:
 #       o freq,fftdata = dofft(pcmdata,fs,alpha): Runs fft on pcmdata with sampling frequency
 #           fs using cosine taper (Tukey) defined by alpha
-#   AudioProcessor Functions:
+#   DrfProcessor Functions:
 #       o __init__(datasource)
 #       o Run(): A separate function called from main.py immediately after initializing the thread to start data 
 #           collection. Here, the callback function which updates the audio stream for WiNRADIO threads is declared
@@ -32,7 +32,7 @@
 
 import numpy as np
 from scipy.io import wavfile as sciwavfile #for wav file reading
-from scipy.signal.windows import tukey #taper generation
+import scipy.signal as sig
 
 import wave #WAV file writing
 
@@ -40,7 +40,6 @@ from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject
 from PyQt5.Qt import QRunnable
 
 import time as timemodule
-import datetime as dt
 
 from traceback import print_exc as trace_error
 
@@ -48,81 +47,121 @@ from shutil import copy as shcopy
 from os.path import exists
 from sys import platform
 
-import pyaudio
 import digital_rf as drf
+from fractions import Fraction
 from pathlib import Path
 
 
-def listaudiodevices():
-    miclist = []
-    indices = []
-    
-    p = pyaudio.PyAudio()
-    for i in range(p.get_device_count()):
-        try:
-            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-                miclist.append(p.get_device_info_by_index(i).get('name'))
-                indices.append(i)
-                #cdict = p.get_device_info_by_index(i)
-                #for ckey in cdict:
-                #    print(f"{ckey}: {cdict[ckey]}\n")
-        except OSError:
-            pass
-    
-    return miclist,indices,p
-            
-
-    
-    
 class DrfInput():
 
     def __init__(self,drfdir):
         drf_path = Path(drfdir).expanduser()
         self.drf_Obj = drf.DigitalRFReader(str(drf_path))
         chans = self.drf_Obj.get_channels()
+
         raw_chan_list = []
-        self.chan_dict = {}
+        self.chan_2sub = {}
+        self.chan_entries = {}
+        self.last_read = {}
+        self.sr_dict = {}
+        self.ref_dict = {}
+        self.time_bnds = (np.inf,-np.inf)
         for ichan in chans:
             props = self.drf_Obj.get_properties(ichan)
+            ref = get_ref(props)
+            sr_f = Fraction(props["sample_rate_numerator"],props["sample_rate_denominator"])
+            bnds = self.drf_Obj.get_bounds(ichan)
             num_sub = props['num_subchannls']
+            self.chan_2sub[ichan] = {ichan:np.arange(num_sub)}
+            self.bnds[ichan] = bnds
+            self.time_bnds = (min(self.time_bnds[0],float(bnds[0]/sr_f)), max(self.time_bnds[1],float(bnds[1]/sr_f)))
+            self.sr_dict[ichan] = sr_f
+            self.ref_dict[ichan] = ref
+            self.last_read[ichan] = (None,None)
             for isub in range(num_sub):
-                self.chan_dict[ichan+":"+str(isub)] = (ichan,isub)
+                self.chan_entries[ichan+":"+str(isub)] = (ichan,isub)
+                
+    def read(self,st_sample,n_sample,chan_entry,adj_bnds = False):
+        """Reads the data from the drf file.
 
 
-
-    def read(self,chan_entry,st_sample,n_sample,adj_bnds = False):
-        ichan,isub = self.chan_dict[chan_entry]
-
+    
+        """
+        if ":" in chan_entry:
+            ichan,isub = self.chan_entries[chan_entry]
+        else:
+            ichan = chan_entry
+            isub = None    
         bnds = self.drf_Obj.get_bounds(ichan)
+        ref = self.ref_dict[ichan]
+        
         if adj_bnds:
             st_sample = max(st_sample,bnds[0])
             n_sample = min(bnds[1],n_sample+st_sample)-st_sample
-        x = self.drf_Obj.read_vector_raw(st_sample,n_sample,ichan,isub)
+        if isub is None:
+            x = self.drf_Obj.read_vector(st_sample,n_sample,ichan)
+        else:
+            x = self.drf_Obj.read_vector(st_sample,n_sample,ichan,isub)
+        self.bnds[ichan] = bnds
+        self.last_read[ichan] = (st_sample,n_sample)
+        x = x/ref
         return x
+    
+    def bnds_update(self):
+        """
+        
+        """
+        chans = list(self.chan2_sub.keys())
+        for ichan in chans:
+            bnds = self.drf_Obj.get_bounds(ichan)
+            sr_f = self.sr_dict[ichan]
+            self.bnds[ichan] = bnds
+            self.time_bnds = (min(self.time_bnds[0],float(bnds[0]/sr_f)), max(self.time_bnds[1],float(bnds[1]/sr_f)))
 
+def get_ref(prop_dict):
+    """Determines the reference to get everything in dBFS. If int divides by the size 2**(nbits) adds 1 half bit for complex data.
 
+    Parameters
+    ----------
+    prop_dict : dict
+        Dictionary from get_properties command on the drf channel.
+
+    Returns
+    -------
+    :float
+        ref level of data.
+    """
+    # HACK if data is a float assuming that FS is power of 1.
+
+    if prop_dict["H5Tget_class"] == 1:
+        return 1.0
+    npow = prop_dict["H5Tget_precision"] - 1.0
+    npow += 0.5 * (prop_dict["H5Tget_size"] - 1.0)
+    return 2**npow
 # =============================================================================
 #  READ SIGNAL FROM WINRADIO, OUTPUT TO PLOT, TABLE, AND DATA
 # =============================================================================
 
 
-class AudioProcessor(QRunnable): 
+class DrfProcessor(QRunnable): 
 
     #initializing current thread (saving variables, reading audio data or contacting/configuring receiver)
-    def __init__(self, p, datasource, savedir, slash, tabID, starttime, fftwindow, dt, alpha, *args,**kwargs):
-        super(AudioProcessor, self).__init__()
+    def __init__(self, datasource,drfdir, tabID, fftbins, n_int,lensig,dt, *args,**kwargs):
 
-        self.p = p #PyAudio instance if calling from mic
-        
-        self.slash = slash
-        
+        super(DrfProcessor, self).__init__()
+
+        self.drfIn = DrfInput(drfdir)
+        chan = [self.drfIn.sr_dict.keys()][0]
+        sr = self.drfIn.sr_dict[chan]
+        self.drf_path = Path(drfdir).expanduser()
         # UI inputs
         self.tabID = tabID
-        self.starttime = starttime
-        self.fftwindow = fftwindow
+        self.fftbins = fftbins
+        self.n_int = n_int
+        self.lensig = lensig
         self.dt = dt
-        self.alpha = alpha
-        
+        self.samp_read = np.arange(int(lensig/dt))*int(sr)
+
         #initializing inner workings
         self.isrunning = False  #set to true while running
         self.signals = ThreadProcessorSignals() # signal connections
@@ -130,48 +169,15 @@ class AudioProcessor(QRunnable):
         self.reason = 0
         
         #output audio (WAV) file name- saving in temporary folder passed from event loop
-        self.wavfilename = savedir + self.slash +  "tempwav_" + str(self.tabID) + '.WAV'
-
-        # identifying whether source is from file or mic
-        self.fromAudio = False
-        self.isfrommic = False
-        if datasource[:3] == 'AAA':
-            self.fromAudio = True
-            
-            chnum = int(datasource[4:9])
-            self.audiofile = datasource[10:]
-            if not exists(self.audiofile):
-                self.terminate(1) #check that file is real, terminate if it isn't
-            
-            shcopy(self.audiofile, self.wavfilename) #copying audio file if datasource = from file
-            self.fs, snd = sciwavfile.read(self.audiofile) #reading file
-            
-            if chnum > 0:
-                self.audiostream = snd[:, chnum-1]
-            else:
-                self.audiostream = snd
-            
-            self.lensignal = len(self.audiostream)
-                
-                
-        elif datasource[:3] == 'MMM':
-            self.fromAudio = False
-            self.audiosourceindex = int(datasource[4:])
-            
-            self.fs = int(np.round(p.get_device_info_by_index(self.audiosourceindex)['defaultSampleRate'])) #get sampling frequency
-            self.sampwidth = 2 #2 bytes per sample corresponds to 16-bit integer
-            self.frametype = pyaudio.paInt16
-            self.audiostream = [0]*100000
-            
-            #setup WAV file to write (if audio or test, source file is copied instead)
-            self.wavfile = wave.open(self.wavfilename,'wb')
-            wave.Wave_write.setnchannels(self.wavfile,1) #single channel output
-            wave.Wave_write.setsampwidth(self.wavfile,2) #sample size configured as int16
-            wave.Wave_write.setframerate(self.wavfile,self.fs)
-            
+        if datasource[:3] == 'streaming':
+            self.streaming=True
         else:
-            self.terminate(1) #terminate before starting: invalid datasource
-        
+            self.streaming = False
+        if not self.drf_path.exists():
+            self.terminate(1)
+                        
+                
+           
         self.isrunning = True
         
         
@@ -197,7 +203,7 @@ class AudioProcessor(QRunnable):
         
         
         #storing FFT settings (this can't happen in __init__ because it might emit updated settings before the slot is connected)
-        self.changethresholds(self.fftwindow,self.dt,self.alpha)
+        self.changethresholds(self.fftwindow,self.dt)
         
         if self.fromAudio: #if source is an audio file 
             self.sampletimes = np.arange(0,self.lensignal/self.fs,self.dt) #sets times to sample from file
@@ -206,7 +212,17 @@ class AudioProcessor(QRunnable):
         else: #using live mic for data source
             self.maxnum = 0 #TODO: ADD INITIALIZATION STUFF HERE
             try:
-                
+
+                def updatedrfbuffer(bufferdata):
+                    try:
+                        if self.isrunning:
+                            pass
+                    except Exception:
+                        trace_error()  
+                        self.terminate(5)
+                        returntype = None
+                    finally:
+                        return (None, returntype)
                 #CALLBACK FUNCTION HERE
                 def updateaudiobuffer(bufferdata, nframes, time_info, status):
                     try:
@@ -243,7 +259,39 @@ class AudioProcessor(QRunnable):
 
             while self.isrunning:
                 i += 1
+                if self.streaming:
+                    self.drfIn.bnds_update()
+                    next_read = {}
+                    for ient, (ist,inum) in self.drfIn.last_read.items():
+                        ichan in self.drfIn.chan_entries[ient][0]
+                        cur_bnds = self.drfIn.bnds[ichan]
+                        old_max = inum+ist
+                        new_st = min(old_max,cur_bnds[-1]-nread)
+                        new_inum = min(cur_bnds[-1]-new_st,nread)
+                        next_read[ient] = (new_st,new_inum)
+                else:
+                    next_read = {}
+                    t_st, nread = get_next_read()
+                    for ient, (ist,inum) in self.drfIn.last_read.items():
+                        ichan in self.drfIn.chan_entries[ient][0]
+                        sr_f = self.drfIn.sr_dict[ichan]
+                        cur_bnds = self.drfIn.bnds[ichan]
+                        old_max = inum+ist
+                        new_st = max(cur_bnds[0],int(sr_f*t_st))
+                        new_inum = min(cur_bnds[-1]-new_st,nread)
+                        next_read[ient] = (new_st,new_inum)
 
+                for ichan,isubs in self.drfIn.chan_2sub.items():
+
+                    ient = ichan+":"+str(isub)
+                    ref = self.ref_dict(ichan)
+                    new_st,newnread = next_read[ient]
+                    d1 = self.drfIn.read(new_st, newnread, ichan)
+                    sr = self.drfIn.sr_dict[ichan]
+                    for isub in isubs:
+                        t_out, f, sxx_int, sxx_med = proc_data(d1[...,isub], self.sr, self.nfft, dt)
+
+                        self.signals.iterated(i,self.maxnum,self.tabID,t_out,f,sxx_int,sxx_med)
                 # finds time from processor start in seconds
                 curtime = dt.datetime.utcnow()  # current time
                 deltat = curtime - self.starttime
@@ -258,7 +306,7 @@ class AudioProcessor(QRunnable):
                         return
                         
                     ctrind = int(np.round(ctime*self.fs))
-                    pmind = int(self.N/2)
+                    pmind = int(self.fftbins/2)
                     
                     if ctrind - pmind >= 0 and ctrind + pmind < self.lensignal:       
                         pcmdata = self.audiostream[ctrind-pmind:ctrind+pmind]
@@ -268,7 +316,7 @@ class AudioProcessor(QRunnable):
 
                 else:
                     ctime = deltat.total_seconds()
-                    pcmdata = np.array(self.audiostream[-self.N:])
+                    pcmdata = np.array(self.audiostream[-self.fftbins:])
                 
                 if pcmdata is not None:
   
@@ -279,7 +327,7 @@ class AudioProcessor(QRunnable):
                     timemodule.sleep(0.08)  # tiny pause to free resources
                     
                 else: #wait for time threshold before getting next point
-                    while (dt.datetime.utcnow() - curtime).total_seconds() < self.dt:
+                    
                         timemodule.sleep(0.1)  
                     
 
@@ -291,76 +339,64 @@ class AudioProcessor(QRunnable):
             
     
             
-            
-    #function to run fft here
-    def dofft(self, pcmdata): 
-        
-        if self.alpha > 0: #applying Tukey taper if necessary
-            if pcmdata.shape[0] == self.taperlen: 
-                ctaper = self.taper
-            else:
-                self.taper = tukey(pcmdata.shape[0], alpha=self.alpha)
-            pcmdata = pcmdata*self.taper
-        
-        # conducting fft, calculating PSD
-        spectra = np.abs(np.fft.fft(pcmdata)**2)/self.df #PSD = |X(f)^2| / df
-        spectra[np.isinf(spectra)] = 1.0E-8 #replacing negative inf values (spectra power=0) with -1
     
-        #limiting data to positive/real frequencies only (and convert to dB)
 
-        spectra = np.log10(spectra[self.keepind])
+
+
+
+
+    # #function to run fft here
+    # def dofft(self, pcmdata): 
         
-        return spectra
+    #     if self.alpha > 0: #applying Tukey taper if necessary
+    #         if pcmdata.shape[0] == self.taperlen: 
+    #             ctaper = self.taper
+    #         else:
+    #             self.taper = tukey(pcmdata.shape[0], alpha=self.alpha)
+    #         pcmdata = pcmdata*self.taper
+        
+    #     # conducting fft, calculating PSD
+    #     spectra = np.abs(np.fft.fft(pcmdata)**2)/self.df #PSD = |X(f)^2| / df
+    #     spectra[np.isinf(spectra)] = 1.0E-8 #replacing negative inf values (spectra power=0) with -1
+    
+    #     #limiting data to positive/real frequencies only (and convert to dB)
+
+    #     spectra = np.log10(spectra[self.keepind])
+        
+    #     return spectra
             
     
         
         
     def calc_settings(self):
         
-        self.N = int(np.round(self.fs*self.fftwindow))
-        if self.N%2: #N must be even
-            self.N += 1
-            
-            
-        #define taper if nonzero alpha
-        if self.alpha > 0:
-            self.taper = tukey(self.N, alpha=self.alpha)
-            self.taperlen = len(self.taper)
-        else:
-            self.taperlen = 0
+        if self.fftbins%2: #N must be even
+            self.fftbins += 1
+
         
-        self.df = self.fs/self.N
-        self.freqs_all = np.array([self.df * n if n < self.N / 2 else self.df * (n - self.N) for n in range(self.N)])
+        self.df = self.fs/self.fftbins
+        self.freqs_all = np.array([self.df * n if n < self.fftbins / 2 else self.df * (n - self.fftbins) for n in range(self.fftbins)])
         self.keepind = np.greater_equal(self.freqs_all,0)
         self.freqs = self.freqs_all[self.keepind]
                 
-        self.signals.statsupdated.emit(self.tabID,self.fs,self.df,self.N,self.freqs)
+        self.signals.statsupdated.emit(self.tabID,self.fs,self.df,self.fftbins,self.freqs)
         
         
         
         
         
     @pyqtSlot(float,float,float)
-    def changethresholds_slot(self,fftwindow,dt,alpha): #update data thresholds for FFT
-        self.changethresholds(fftwindow,dt,alpha)
+    def changethresholds_slot(self,fftwindow): #update data thresholds for FFT
+        self.changethresholds(fftwindow)
         
         
     
-    def changethresholds(self,fftwindow,dt,alpha): #update data thresholds for FFT
+    def changethresholds(self,fftwindow): #update data thresholds for FFT
         if fftwindow <= 1:
             self.fftwindow = fftwindow
         else:
             self.fftwindow = 1
-            
-        self.dt = dt
-        
-        if alpha > 1:
-            self.alpha = 1
-        elif alpha < 0:
-            self.alpha = 0
-        else:
-            self.alpha = alpha
-            
+
         self.calc_settings()
         
         
@@ -387,7 +423,56 @@ class AudioProcessor(QRunnable):
         
         
         
-        
+def proc_data(d1, sr, nfft, dt):
+    """Creates a STI, min median and max spectra.
+
+    Parameters
+    ----------
+    d1 : array_like
+        Input data.
+    sr : float
+        Sampling rate in Hz
+    nfft : int
+        Number of FFT bins in spectra.
+    dt : float
+        For each time instance of the STI will integrate over this period.
+
+    Returns
+    -------
+    t_out : array_like
+        Time array output in seconds from STI.
+    f : array_like
+        Frequency array of spectrum in Hz.
+    sxx_int : array_like
+        STI data
+    sxx_med : array_like
+        Median across time of the STI
+    sxx_min : array_like
+        Minimum across the time of the STI
+    sxx_max : array_like
+        Maximum across the time of the STI.
+    """
+    win = sig.get_window(("kaiser", 1.7), nfft)
+    f, t, sxx = sig.spectrogram(
+        d1, sr, window=win, detrend=False, return_onesided=False, scaling="spectrum"
+    )
+    n_int = int(dt / (t[1] - t[0]))
+    n1 = np.arange(0, len(t), n_int)
+
+    sxx_int = np.zeros((nfft, len(n1) - 1), dtype=sxx.dtype)
+    for i in range(len(n1[:-1])):
+        sxx_chunk = sxx[:, n1[i] : n1[i + 1]]
+        sxx_int[:, i] = np.mean(sxx_chunk, axis=-1)
+
+    t_out = t[n1][:-1]
+    f = np.fft.fftshift(f)
+    sxx_int = np.fft.fftshift(sxx_int, axes=0)
+
+    sxx_med = np.median(sxx_int, axis=-1)
+
+
+    return t_out, f, sxx_int, sxx_med
+     
         
 #TODO: CONFIGURE THESE
 #initializing signals for data to be passed back to main loop
